cafdfdb229430ee5b7521ba5913d8ee7f6ea81eb
Pablo Pineda <pinedapablo6718@gmail.com>
2024-05-14 22:58:10 -0500

    docs: add info to README

--- SistemaContableApp/tests/updateUserTests/updateUserPermissionTests.py
+++ SistemaContableApp/tests/updateUserTests/updateUserPermissionTests.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group
 from SistemaContableApp.models import  *
 from SistemaContableApp.views import  *
 
-a
+
 class updateUserPermissionTestCase(TestCase):
 
     def setUp(self):
